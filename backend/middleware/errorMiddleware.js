3a8ba000bc04afdfed80bd8a02f2512ec42a16ee
CollinSchuster! <collinraeschuster@gmail.com>
2023-08-11 12:32:32 +0800

    Goals controller and routes setup

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+// all middleware is is just functions that execute during the request response cycle
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500
+
+  res.status(statusCode)
+
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack, // only need this in development mode
+  })
+}
+
+module.exports = {
+  errorHandler
+}
